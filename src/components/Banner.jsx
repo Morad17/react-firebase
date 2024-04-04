4de9dfbc8a1818aaf73988cef1d33f031d7541af
Morad17 <morad123@hotmail.co.uk>
2024-04-04 03:13:52 +0100

    creating banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import Login from '../components/Login'
+import explore from '../assets/icons/explore.png'
+import rate from '../assets/icons/rate.png'
+import upload from '../assets/icons/upload.png'
+
+
+const Banner = () => {
+
+    const currentUser = JSON.parse(localStorage.getItem("user"))
+
+  return (
+    <div className="banner">
+        <div className="icon-group">
+          <div className="icons">
+            <img src={explore} alt="" />
+            <h3>Explore</h3>
+            <p>Explore A Large Gallery Of Travel Photoss From Around The World</p>
+          </div>
+          <div className="icons">
+            <img src={rate} alt="" /> 
+            <h3>Rate</h3>
+            <p>Rate Your Favourite Photo's And Build Your Own Collection</p>
+          </div>
+          <div className="icons">
+            <img src={upload} alt="" />
+            <h3>Upload</h3>
+            <p>Upload Your Own Photo's For Others To Admire</p>
+          </div>
+        </div>
+        <div className="login-group">
+            {currentUser ? "" :
+            <div className="login-and-register">
+                <div className="login-section">
+                    <h2>Login To Get Started</h2>
+                    <Login />
+                </div>
+                <div className="create-user-section">
+                    <h2>Don't have an accout? Create A New Account Here</h2>
+                    <button>
+                    <Link to="/register">Register</Link>
+                    </button>
+                </div>
+            </div>
+            }
+        </div>
+        
+      </div>
+  )
+}
+
+export default Banner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Login from '../components/Login'
+import Banner from '../components/Banner'
 
 const Home = () => {
 
-  const currentUser = JSON.parse(localStorage.getItem("user"))
   return (
     <div className="home">
-      {currentUser ? "" :
-        <div className="login-and-registration">
-           <div className="login-section">
-            <h2>Login To Get Started</h2>
-            <Login />
-          </div>
-          <div className="create-user-section">
-            <h2>Don't have an accout? Create A New Account Here</h2>
-            <button>
-              <Link to="/register">Register</Link>
-            </button>
-          </div>
-        </div>
-       
-      }
+      <Banner />
+      
       
       
     </div>
