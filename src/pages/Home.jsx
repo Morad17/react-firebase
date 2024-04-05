56c02327ff28dc51ea5349436016ed578c091b8f
Morad17 <morad123@hotmail.co.uk>
2024-04-05 01:11:18 +0100

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Banner from '../components/Banner'
+import { getAuth } from 'firebase/auth'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../Firebase'
 
 const Home = () => {
 
+  const [photos, getPhotos] = useState()
+
+  const auth = getAuth()
+
+  useEffect(()=> {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, ))
+    }
+  }, [])
+
   return (
+    
     <div className="home">
       <Banner />
-      
-      
-      
+      <div className="all-photos">
+        {
+
+        }
+      </div>
     </div>
   )
 }
