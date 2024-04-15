7b19249c25c839b5fcb10d5dfd73f448ffd75f62
Morad17 <morad123@hotmail.co.uk>
2024-04-15 22:05:34 +0100

    created admin sidebar

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import StockIcon from '../assets/images/stock-profile-icon.png'
+import AllPhotos from '../assets/icons/all-photos.png'
+import Dashboard from '../assets/icons/dashboard.png'
+import AddPhoto from '../assets/icons/new-photo.png'
+import Favourite from '../assets/icons/favourite.png'
 
 const AdminSidebar = () => {
   return (
     <nav className="admin-sidebar">
+        <div className="profile-photo">
+            <img src={StockIcon} alt="" />
+        </div>
         <ul>
-            <li>
-                <p>Dashboard</p>
-                <p>All Photos</p>
-            </li>
+            <li className="sidebar-links">
+                <Link to="/admin-page">
+                <img className="dashboard-icon"src={Dashboard} alt="" />
+                    Dashboard</Link></li>
+            <li className="sidebar-links">
+                <Link>
+                <img className="all-photos-icon" src={AllPhotos} alt="" />
+                    All Photos</Link></li>
+            <li className="sidebar-links">
+                <Link>
+                <img className="add-photos-icon"src={AddPhoto} alt="" />  
+                Add Photos</Link></li>
+            <li className="sidebar-links">
+                <Link>
+                <img className="favourite-icon"src={Favourite} alt="" />
+                    Favourite Photos</Link></li>
         </ul>
     </nav>
   )
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import AddData from '../components/AddData'
+import AdminSidebar from '../components/AdminSidebar'
 
 const AdminPage = () => {
   return (
     <div>
-      <AddData />
+      <AdminSidebar />
     </div>
   )
 }
