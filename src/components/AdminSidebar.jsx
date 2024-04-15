ea2ea223c8e7afe412ceabf9675b93bc506f42af
Morad17 <morad123@hotmail.co.uk>
2024-04-15 14:30:13 +0100

    updates

---
+++ src/components/AdminSidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AdminSidebar = () => {
+  return (
+    <nav className="admin-sidebar">
+        <ul>
+            <li>
+                <p>Dashboard</p>
+                <p>All Photos</p>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default AdminSidebar
