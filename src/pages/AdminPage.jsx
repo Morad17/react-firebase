2d94fdfca4e21f745dd0edf24625bc1c9966c669
Morad17 <morad123@hotmail.co.uk>
2024-04-17 08:50:25 +0100

    creating dashboard

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,8 +4,24 @@ import AdminSidebar from '../components/AdminSidebar'
 
 const AdminPage = () => {
   return (
-    <div>
+    <div className="admin-page">
       <AdminSidebar />
+      <div className="dashboard">
+        <section className="impressions">
+          <h3>Stats</h3>
+          <div className="photos-submitted-card">
+            test
+          </div>
+          <div className="views-card"></div>
+          <div className="likes-card"></div>
+          <div className="subs-card"></div>
+        </section>
+        <section className="top-picture">
+        </section>
+        <section className="stat-graph">
+
+        </section>
+      </div>
     </div>
   )
 }
