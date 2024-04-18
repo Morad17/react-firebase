43e6577cefc9d0710f1ff0e62aa06fedaa3710b6
Morad17 <morad123@hotmail.co.uk>
2024-04-18 11:30:21 +0100

    setting up dashboard

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import AddData from '../components/AddData'
 import AdminSidebar from '../components/AdminSidebar'
+import { Link } from 'react-router-dom'
+
+import downloaded from '../assets/icons/downloaded.png'
 
 const AdminPage = () => {
   return (
@@ -8,18 +11,40 @@ const AdminPage = () => {
       <AdminSidebar />
       <div className="dashboard">
         <section className="impressions">
-          <h3>Stats</h3>
-          <div className="photos-submitted-card">
-            test
+          <div className="views-card card-vi">
+            <h3 className="card-title">All Views</h3>
+            <img src="" alt="" />
+            <p>100</p>
+            <Link><p>All Photos</p></Link>
           </div>
-          <div className="views-card"></div>
-          <div className="likes-card"></div>
-          <div className="subs-card"></div>
+          <div className="likes-card card-vi">
+            <h3 className="card-title card-vi">All Views</h3>
+            <img src={downloaded} alt="" />
+            <p>100</p>
+            <Link><p>All Photos</p></Link>
+          </div>
+          <div className="subs-card card-vi">
+            <h3 className="card-title card-vi">All Views</h3>
+            <img src="" alt="" />
+            <p>100</p>
+            <Link><p>All Photos</p></Link>
+          </div>
         </section>
         <section className="top-picture">
         </section>
         <section className="stat-graph">
 
+        </section>
+        <section className="user-metrics">
+          <div className="photos-submitted-card">
+              test
+            </div>
+          <div className="photos-downloaded-card">
+
+          </div>
+          <div className="photos-shared-card">
+
+          </div>
         </section>
       </div>
     </div>
