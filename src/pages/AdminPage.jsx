f8763429fdd581500ce48d8e93e263da5cfaae24
Morad17 <morad123@hotmail.co.uk>
2024-04-18 22:40:04 +0100

    styling dashboard

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,7 +3,17 @@ import AddData from '../components/AddData'
 import AdminSidebar from '../components/AdminSidebar'
 import { Link } from 'react-router-dom'
 
+import topPhoto from '../assets/images/stock-top-picture.JPG'
 import downloaded from '../assets/icons/downloaded.png'
+import dashboard from '../assets/icons/dashboard.png'
+import likes from '../assets/icons/likes.png'
+import subs from '../assets/icons/subs.png'
+import views from '../assets/icons/views.png'
+import uploaded from '../assets/icons/uploaded.png'
+import shared from '../assets/icons/shared.png'
+
+
+
 
 const AdminPage = () => {
   return (
@@ -12,38 +22,76 @@ const AdminPage = () => {
       <div className="dashboard">
         <section className="impressions">
           <div className="views-card card-vi">
-            <h3 className="card-title">All Views</h3>
-            <img src="" alt="" />
-            <p>100</p>
-            <Link><p>All Photos</p></Link>
+            <div className="top-line">
+              <h3 className="card-title">Total Views</h3>
+              <img src={views} alt="" />
+            </div>
+            <p className="card-number">100</p>
+            <Link><p className="card-link">All Photos</p></Link>
           </div>
           <div className="likes-card card-vi">
-            <h3 className="card-title card-vi">All Views</h3>
-            <img src={downloaded} alt="" />
-            <p>100</p>
-            <Link><p>All Photos</p></Link>
+            <div className="top-line">
+              <h3 className="card-title">Total Likes</h3>
+              <img src={likes} alt="" />
+            </div>
+           
+            <p className="card-number">100</p>
+            <Link><p className="card-link">All Photos</p></Link>
           </div>
           <div className="subs-card card-vi">
-            <h3 className="card-title card-vi">All Views</h3>
-            <img src="" alt="" />
-            <p>100</p>
-            <Link><p>All Photos</p></Link>
+            <div className="top-line">
+              <h3 className="card-title">Total Subs</h3>
+            <img src={subs} alt="" />
+            </div>
+            
+            <p className="card-number">100</p>
+            <Link><p className="card-link">All Photos</p></Link>
           </div>
         </section>
         <section className="top-picture">
+        <div className="top-picture-card">
+          <h3>Name</h3>
+          <img src={topPhoto} alt="" />
+          <div className="top-photo-info">
+            <p className="views"><span>100</span> Views</p>
+            <p className="likes"><span>200</span> Likes</p> 
+          </div>
+          <div className="top-photo-info-2">
+            <p className="downloads"><span>50</span>Downloads</p>
+            <p className="shared"><span>50 </span>Shares</p>
+          </div>
+          
+         
+         
+        </div>
         </section>
         <section className="stat-graph">
 
         </section>
         <section className="user-metrics">
-          <div className="photos-submitted-card">
-              test
+          <div className="photos-uploaded-card card-vi">
+            <div className="top-line">
+              <h3 className="card-title">Photos Uploaded</h3>
+              <img src={uploaded} alt="" />
             </div>
-          <div className="photos-downloaded-card">
-
+            <p className="card-number">100</p>
+            <Link><p className="card-link">All Photos</p></Link>
+            </div>
+          <div className="photos-downloaded-card card-vi">
+            <div className="top-line">
+              <h3 className="card-title">Photos Downloaded</h3>
+              <img src={downloaded} alt="" />
+            </div>
+            <p className="card-number">100</p>
+            <Link><p className="card-link">All Photos</p></Link>
           </div>
-          <div className="photos-shared-card">
-
+          <div className="photos-shared-card card-vi">
+            <div className="top-line">
+              <h3 className="card-title">Photos Shared</h3>
+              <img src={shared} alt="" />
+            </div>
+            <p className="card-number">100</p>
+            <Link><p className="card-link">All Photos</p></Link>
           </div>
         </section>
       </div>
